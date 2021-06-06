4a9e968bbb2c54fd65cc9e44b8bb1a909a0f35c1
glassinatorn <gusten1995@gmail.com>
2021-06-06 23:39:45 +0200

    Made the toggle

--- lua/light-toggle/init.lua
+++ lua/light-toggle/init.lua
@@ -1,12 +1,17 @@
 Light_switch = true	    -- defining default value
 
-local function toggle(...)
+local function toggle()
     -- TODO:
     -- implement the function []
     -- make sure it works with default global variable []
 
     if Light_switch then
+	vim.g.colors_name = "snazzy"
+	Light_switch = true	    -- defining default value
 	print(vim.g.colors_name)
+    else
+	vim.g.colors_name = "nord"
+	Light_switch = false	    -- defining default value
     end
 end
 
