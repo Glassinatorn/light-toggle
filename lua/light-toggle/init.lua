c3cd20ebf75fc436641d85ec02922cc75fed51ea
glassinatorn <gusten1995@gmail.com>
2021-06-15 01:28:20 +0200

    Fixed function and changed second color

--- lua/light-toggle/init.lua
+++ lua/light-toggle/init.lua
@@ -1,13 +1,15 @@
+Toggle_color_1 = "qui"
+Toggle_color_2 = "nord"
+
 local function toggle()
     -- TODO:
     -- implement the function []
     -- make sure it works with default global variable []
 
-    if vim.g.colors_name == "snazzy" then
-	vim.g.colors_name = "snazzy"
+    if vim.g.colors_name == Toggle_color_1 then
 	vim.cmd [[colorscheme nord]]
-    elseif vim.g.colors_name == "nord" then
-	vim.cmd [[colorscheme snazzy]]
+    elseif vim.g.colors_name == Toggle_color_2 then
+	vim.cmd [[colorscheme yui]]
     end
 end
 
