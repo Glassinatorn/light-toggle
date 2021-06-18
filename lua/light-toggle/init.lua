044e44766141a375314e235f836d8f5b87a6252d
Glassinatorn <gusten1995@gmail.com>
2021-06-19 00:12:26 +0200

    Removed attempt at defaulting colorschemes

--- lua/light-toggle/init.lua
+++ lua/light-toggle/init.lua
@@ -1,21 +1,13 @@
--- sets the globals if they do not excist
-if vim.api.nvim_get_var("Toggle_color_1") == "" then
-    vim.api.nvim_set_var("Toggle_color_1", "nord") -- temporary color
-end
-
-if vim.api.nvim_get_var("Toggle_color_2") == "" then
-    vim.api.nvim_set_var("Toggle_color_2", "yui") -- temporary color
-end
-
 local function toggle()
     -- TODO:
     -- implement the function [x]
-    -- make sure it works with default global variable []
+    -- make sure it works with global variable [x]
 
+    -- getting color settings
     local color1 = vim.api.nvim_get_var("Toggle_color_1")
     local color2 = vim.api.nvim_get_var("Toggle_color_2")
 
-
+    -- changing colorscheme
     if vim.g.colors_name == color1 then
 	vim.api.nvim_command("colorscheme yui")
     elseif vim.g.colors_name == color2 then
