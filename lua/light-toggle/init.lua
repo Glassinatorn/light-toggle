387df69b31ff52a4348646d6728f7d9d98b3386b
glassinatorn <gusten1995@gmail.com>
2021-06-18 13:06:38 +0200

    Changed way of getting and setting values

--- lua/light-toggle/init.lua
+++ lua/light-toggle/init.lua
@@ -1,15 +1,22 @@
-Toggle_color_1 = "yui"
-Toggle_color_2 = "nord"
+-- sets the globals if they do not excist
+if vim.api.nvim_get_var("Toggle_color_1") == "" then
+    vim.api.nvim_set_var("Toggle_color_1", "nord") -- temporary color
+end
+
+if vim.api.nvim_get_var("Toggle_color_2") == "" then
+    vim.api.nvim_set_var("Toggle_color_2", "yui") -- temporary color
+end
 
 local function toggle()
     -- TODO:
     -- implement the function [x]
     -- make sure it works with default global variable []
 
+
     if vim.g.colors_name == Toggle_color_1 then
-	vim.cmd [[colorscheme nord]]
+	vim.api.nvim_command("colorscheme nord")
     elseif vim.g.colors_name == Toggle_color_2 then
-	vim.cmd [[colorscheme yui]]
+	vim.api.nvim_command("colorscheme yui")
     end
 end
 
